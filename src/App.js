8d9c9dc24ec987fc8aaaa7ecdd684cd82f21665a
Sultan <ffkirito6@gmail.com>
2023-09-11 21:41:46 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react';
+import { HashRouter as Router } from 'react-router-dom';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
+import Routes from './components/Routes';
+
+const App = () => {
+  const [coffees, setCoffees] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(process.env.PUBLIC_URL + '/db.json');
+        if (!response.ok) {
+          throw new Error('Failed to fetch data');
+        }
+        const data = await response.json();
+        setCoffees(data.coffee);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <Router>
+      <div className="app">
+        <Header />
+        <main>
+          <Routes coffees={coffees} /> 
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  );
+};
+
+export default App;
